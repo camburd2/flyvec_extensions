fd11609bad524bb16713bb70615b85ca1c7f8da7
naturalhg <n@mamoe.net>
2024-12-02 15:59:47 -0600

    calc_print_sim_words_in_context

--- utils.py
+++ utils.py
@@ -16,8 +16,18 @@ class Encoder:
             words = input
         return [self.vocab[word] for word in words]
 
-    def one_hot(self, input, create_target_vector=False):
-        tokens = self.tokenize(input)
+    def tokenize_unsafe(self, input):
+        if isinstance(input, str):
+            words = input.split()
+        else:
+            words = input
+        return [self.vocab.get(word, self.vocab['<unk>']) for word in words]
+
+    def one_hot(self, input, create_target_vector=False, unsafe=False):
+        if unsafe:
+            tokens = self.tokenize_unsafe(input)
+        else:
+            tokens = self.tokenize(input)
         encoded = torch.zeros(self.vocab_size, dtype=torch.float32)
         encoded[tokens] = 1
         if create_target_vector:
@@ -27,7 +37,6 @@ class Encoder:
             #combine with encode+target
             encoded = torch.cat((encoded, target))
         return encoded
-
 
 
 def calc_sim(v1, v2):
@@ -40,14 +49,9 @@ def calc_sim(v1, v2):
     return similarity
 
 
-def calc_print_sim_words(vocab, word_counts, model, word, hash_len=50, top_N=15, create_target_vector=False):
-    enc = Encoder(vocab)
 
-    enc_target_word = enc.one_hot(word, create_target_vector=create_target_vector)
-    hash_target = model.get_embedding(enc_target_word, hash_len)
-
+def print_sim_words(vocab, enc, model, hash_target, hash_len, create_target_vector, word_counts, top_N=15):
     sims = []
-
     # Calculate  similarity between target and the rest of vocab
     for word in vocab.keys():
         enc_word = enc.one_hot(word, create_target_vector=create_target_vector)
@@ -63,6 +67,26 @@ def calc_print_sim_words(vocab, word_counts, model, word, hash_len=50, top_N=15,
     print("-" * 35)
     for word, sim in top_N:
         print(f"{word:<15} {sim:>9.3f} {word_counts[word]:>10}")
+
+def calc_print_sim_words(vocab, word_counts, model, word, hash_len=50, top_N=15, create_target_vector=False):
+    enc = Encoder(vocab)
+
+    enc_target_word = enc.one_hot(word, create_target_vector=create_target_vector)
+    hash_target = model.get_embedding(enc_target_word, hash_len)
+
+    print_sim_words(vocab, enc, model, hash_target, hash_len, create_target_vector, word_counts, top_N=top_N)
+
+
+def calc_print_sim_words_in_context(vocab, word_counts, model, sentence, target, hash_len = 50, top_N = 15, create_target_vector=False):
+    enc = Encoder(vocab)
+    end_target_word = enc.one_hot(sentence, create_target_vector=False, unsafe=True)
+    if create_target_vector:
+        target_vector = enc.one_hot(target, create_target_vector=False)
+        end_target_word = torch.cat((end_target_word, target_vector))
+
+    hash_target = model.get_embedding(end_target_word, hash_len)
+
+    print_sim_words(vocab, enc, model, hash_target, hash_len, create_target_vector, word_counts, top_N=top_N)
 
 
 def save_model(model, filepath):
